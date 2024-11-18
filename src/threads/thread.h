21fc6b8cbd995934631b4acc0ae9d654f827ccee
azazo1 <azazo1@qq.com>
2024-11-18 18:38:40 +0800

    elem intro

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -92,7 +92,7 @@ struct thread
     struct list_elem allelem;           /* List element for all threads list. */
 
     /* Shared between thread.c and synch.c. */
-    struct list_elem elem;              /* List element. */
+    struct list_elem elem;              /* List element. */ /* 只能用于 ready_list. */
 
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
